47e54144ebd067264cd28e126ff8f45fecfc9a84
Prashant <prashant.sahni5@gmail.com>
2022-01-15 12:21:52 +0530

    Correct indentation

--- src/components/Category.component.tsx
+++ src/components/Category.component.tsx
@@ -7,95 +7,67 @@ type Props = OwnProps;
 type State = {};
 
 const Seat = ({ id, label, book }: {id: string, label: string, book: () => void}) => {
-	return (
-		<div className='seat' onClick={() => book()} data-id={id}>{label}</div>
-	)
+  return (
+    <div className='seat' onClick={() => book()} data-id={id}>{label}</div>
+  )
 }
 
 const Row = ({ seats }: { seats: JSX.Element[]}) => {
-	return (
-		<div className='row'>
-			{ seats }
-		</div>
-	)
+  return (
+    <div className='row'>
+    { seats }
+    </div>
+  )
 }
 
 class Category extends React.Component<Props, State>  {
 
-	constructor(props: Props) {
-		super(props);
-		this.book.bind(this);
-	}
+  constructor(props: Props) {
+    super(props);
+    this.book.bind(this);
+  }
 
-	book(id: string) {
-		console.log('########', id);
-	}
+  book(id: string) {
+    console.log('########', id);
+  }
 
-/*
-	renderRows() {
-		let rows = '';
-		let seat_no = 0;
-		let row_no = 0;
-		
-		const { rows: total_rows, seats_per_row } =  this.props;
-		const total_seats = total_rows*seats_per_row;
-		
-		for (let i = 0; i < total_seats; i++) {
-			row_no += (i % seats_per_row) === 0 ? 1: 0;
-			seat_no = (i % seats_per_row) === 0 ? 0 : (seat_no + 1);
-			if ((i+1) % seats_per_row === 1) {
-				rows += ("<div class= 'row'>Row " + row_no);
-			}
-			
-			const label = String.fromCharCode(seat_no+ 65);
-			const seat = ReactDOMServer.renderToString(<Seat book={this.book} label={label} row_no={row_no}/>);
-			rows += seat
-			
-			if ((i+1) % seats_per_row === 0) {
-				rows += ("</div>");
-			}
-		}
-    return ReactHtmlParser(rows);
-	}
-*/
+  renderRows2(category_id: string) {
+    let rows = [];
+    let seats = [];
+    let seat_no = 0;
+    let row_no = 0;
 
-	renderRows2(category_id: string) {
-		let rows = [];
-		let seats = [];
-		let seat_no = 0;
-		let row_no = 0;
-		
-		const { rows: total_rows, seats_per_row } =  this.props;
-		const total_seats = total_rows*seats_per_row;
-		
-		for (let i = 0; i < total_seats; i++) {
-			row_no += (i % seats_per_row) === 0 ? 1: 0;
-			seat_no = (i % seats_per_row) === 0 ? 0 : (seat_no + 1);
-			
-			const label = String.fromCharCode(seat_no+ 65);
-			const id = category_id + 'R' + row_no + label;
+    const { rows: total_rows, seats_per_row } =  this.props;
+    const total_seats = total_rows*seats_per_row;
 
-			const seat = (<Seat key={id} book={() => this.book(id)} label={label} id={id}/>)
-			seats.push(seat);
-			
-			if ((i+1) % seats_per_row === 0) {
-				rows.push(<Row key={i+1} seats={seats}/>);
-				seats = [];
-			}
-		}
+    for (let i = 0; i < total_seats; i++) {
+      row_no += (i % seats_per_row) === 0 ? 1: 0;
+      seat_no = (i % seats_per_row) === 0 ? 0 : (seat_no + 1);
+      
+      const label = String.fromCharCode(seat_no+ 65);
+      const id = category_id + 'R' + row_no + label;
+
+      const seat = (<Seat key={id} book={() => this.book(id)} label={label} id={id}/>)
+      seats.push(seat);
+      
+      if ((i+1) % seats_per_row === 0) {
+        rows.push(<Row key={i+1} seats={seats}/>);
+        seats = [];
+      }
+    }
     return rows;
-	}
+  }
 
-	render() {
-		return (
-			<div>
-				<span className='category-name'>{ this.props.name }</span>
-				<div className="rows">
-					{ this.renderRows2(this.props.id) }
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <span className='category-name'>{ this.props.name }</span>
+        <div className="rows">
+          { this.renderRows2(this.props.id) }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Category;
--- src/components/SeatsByCategory.component.tsx
+++ src/components/SeatsByCategory.component.tsx
@@ -3,24 +3,23 @@ import { ISeatCategory } from '../models/SeatCategory';
 import Category from  './Category.component';
 
 interface OwnProps {
-	seatsCategories: ISeatCategory[]
+  seatsCategories: ISeatCategory[]
 }
 
 type Props = OwnProps;
 type State = {};
 
 class SeatsByCategory extends React.Component<Props, State> {
-
-	render() {
-		const { seatsCategories } = this.props;
-		return (
-			<React.Fragment>
-				{
-					seatsCategories.map((seatsCategory) => <Category key={seatsCategory.id} {...seatsCategory}/>)
-				}
-			</React.Fragment>
-		)
-	}
+  render() {
+    const { seatsCategories } = this.props;
+    return (
+      <React.Fragment>
+        {
+          seatsCategories.map((seatsCategory) => <Category key={seatsCategory.id} {...seatsCategory}/>)
+        }
+      </React.Fragment>
+    )
+  }
 }
 
 export default SeatsByCategory;
